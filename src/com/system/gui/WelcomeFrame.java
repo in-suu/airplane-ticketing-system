b139f581ea814f45d1fbf1e121adbc5a89bb31d7
Yun-Tzu C. Cosing <cosingyuntzu@gmail.com>
2026-04-20 08:22:30 +0800

    Update WelcomeFrame.java

--- src/com/system/gui/WelcomeFrame.java
+++ src/com/system/gui/WelcomeFrame.java
@@ -19,9 +19,6 @@ public class WelcomeFrame extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 
-	/**
-	 * Launch the application.
-	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -35,31 +32,24 @@ public class WelcomeFrame extends JFrame {
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
 	public WelcomeFrame() {
 		setTitle("Eroplanong Papel - Welcome");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		// Window size: 800x450 (Landscape)
 		setBounds(100, 100, 800, 450); 
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
-		// 1. Gawa muna ng Button (Para mauna siya sa "Stack")
-		// --- BUTTON SETUP ---
+	
 		JButton btnNewButton = new JButton("Start Your Journey");
 		btnNewButton.setFont(new Font("Segoe UI", Font.BOLD, 15));
 		btnNewButton.setBackground(DataManager.MIDNIGHT_BLUE);
 		btnNewButton.setForeground(Color.WHITE);
 
-		// UI Cleaning (The "Professional" Touches)
 		btnNewButton.setOpaque(true);
 		btnNewButton.setContentAreaFilled(true);
 		btnNewButton.setBorderPainted(false);
-		btnNewButton.setFocusPainted(false); // TINATANGGAL ANG GRAY BOX SA TEXT
+		btnNewButton.setFocusPainted(false); 
 		btnNewButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
 
 		btnNewButton.setBounds(310, 310, 180, 45);
@@ -68,18 +58,16 @@ public class WelcomeFrame extends JFrame {
 		// --- TRANSITION LOGIC ---
 		btnNewButton.addActionListener(e -> {
 		    new FlightViewFrame().setVisible(true); // Open next screen
-		    this.dispose(); // Kill current screen to save RAM
+		    this.dispose(); 
 		});
-		
-		// I-paste ito bago ang lblBackground.setIcon(finalIcon);
+	
 		ImageIcon originalIcon = new ImageIcon(WelcomeFrame.class.getResource("/com/system/images/upscaled_malabo.png"));
 		Image img = originalIcon.getImage();
 		Image scaledImg = img.getScaledInstance(784, 411, Image.SCALE_SMOOTH);
-		ImageIcon finalIcon = new ImageIcon(scaledImg); // Eto yung hinahanap na variable!
+		ImageIcon finalIcon = new ImageIcon(scaledImg); 
 
-		// --- BACKGROUND (Make sure this is LAST in the code) ---
+	
 		JLabel lblBackground = new JLabel("");
-		// Use the same scaling logic we did last night...
 		lblBackground.setIcon(finalIcon); 
 		lblBackground.setBounds(0, 0, 784, 411);
 		contentPane.add(lblBackground);
